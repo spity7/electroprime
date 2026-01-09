80a630c83404203b42676d49ded4dc02c2ee61ae
hasan sbeity <th.rider.clan@gmail.com>
2026-01-09 18:39:17 +0200

    initial commit

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: "standalone",
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
